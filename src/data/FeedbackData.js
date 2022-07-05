ac90255d6836a59e6e027b750693cb300a553506
Stanislaw Cruz <stanycruz@outlook.com>
2022-07-05 09:33:02 -0300

    feat: add global state

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ultrices diam. Sed ac consequat ante. Etiam eu purus at neque semper rutrum eget vel enim. Suspendisse ac lobortis odio.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Vestibulum faucibus nibh eu felis placerat tempus. Etiam luctus purus est, vel rhoncus massa interdum ac. Proin nec nisl purus. Sed eros velit, egestas at elementum in, tristique vel ante. Vestibulum quis porttitor nisl, ac aliquet enim.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Curabitur auctor sed nunc id tempor. Integer ligula tortor, pellentesque faucibus ex sed, ultricies tincidunt felis. Pellentesque viverra mattis vestibulum. Fusce in enim magna. Ut tempus, elit nec maximus posuere, elit nunc faucibus neque, a aliquam urna purus eget urna.',
+  },
+]
+
+export default FeedbackData
