5e6c71b1bf0d167cf7baf1f4527afe4e20d9d63a
Stanislaw Cruz <stanycruz@outlook.com>
2022-07-28 19:08:06 -0300

    refactor: get global state with the useContext hook

--- src/App.js
+++ src/App.js
@@ -33,9 +33,8 @@ function App() {
             <Route exact path='/' element={
               <>
                 <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete=
-                {deleteFeedback} />
+                <FeedbackStats />
+                <FeedbackList handleDelete={deleteFeedback} />
                 <AboutIconLink />
               </>
             }
