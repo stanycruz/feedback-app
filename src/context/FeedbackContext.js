71a49d5bc657c93d2b5a2a377b1874ace3486c12
Stanislaw Cruz <stanycruz@outlook.com>
2022-07-31 15:27:53 -0300

    feat: add edit feedback event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,24 +22,44 @@ export const FeedbackProvider = ({children}) => {
     },
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
 
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
-  return <FeedbackContext.Provider value={{
-    feedback,
-    deleteFeedback,
-    addFeedback,
-  }}>
-    {children}
-  </FeedbackContext.Provider>
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
+  return (
+    <FeedbackContext.Provider 
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  )
 }
 
 export default FeedbackContext
